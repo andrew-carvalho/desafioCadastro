b69327e229d328dd2ca7030580e7b29a2c510f34
Andrew Ignácio <andrewignacio@outlook.com.br>
2025-03-26 20:50:06 -0300

    feat: add file with questions

--- src/main/java/com/andrew/application/Program.java
+++ src/main/java/com/andrew/application/Program.java
@@ -1,9 +1,28 @@
 package com.andrew.application;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Program {
 
     public static void main(String[] args) {
+        printMenu();
+    }
 
+    public static void printMenu() {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("formulario.txt"))) {
+            String fileLine;
+
+            while ((fileLine = bufferedReader.readLine()) != null) {
+                System.out.println(fileLine);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
