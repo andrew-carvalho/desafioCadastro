40b03de6ea1d0e093ba3f798b5149165bf3c4540
Andrew Ignácio <andrewignacio@outlook.com.br>
2025-03-26 21:07:20 -0300

    feat: add menu

--- src/main/java/com/andrew/application/Program.java
+++ src/main/java/com/andrew/application/Program.java
@@ -4,23 +4,82 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Program {
 
+    public static Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
+        printFormulario();
         printMenu();
     }
 
-    public static void printMenu() {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("formulario.txt"))) {
+    public static StringBuilder readFileContent(String path) {
+        StringBuilder fileContent = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
             String fileLine;
             while ((fileLine = bufferedReader.readLine()) != null) {
-                System.out.println(fileLine);
+                fileContent.append(fileLine).append("\n");
             }
         } catch (FileNotFoundException e) {
             System.out.println("Arquivo não encontrado! " + e.getMessage());
         } catch (IOException e) {
             System.out.println("Erro interno! " + e.getMessage());
+        }
+
+        return fileContent.delete(fileContent.length() - 1, fileContent.length());
+    }
+
+    public static void printFormulario() {
+        StringBuilder fileContent = readFileContent("formulario.txt");
+        System.out.println(fileContent);
+        System.out.println();
+    }
+
+    public static void printMenu() {
+        while (true) {
+            System.out.println("1. Cadastrar um novo pet");
+            System.out.println("2. Alterar os dados do pet cadastrado");
+            System.out.println("3. Deletar um pet cadastrado");
+            System.out.println("4. Listar todos os pets cadastrados");
+            System.out.println("5. Listar pets por algum critério (idade, nome, raça)");
+            System.out.println("6. Sair");
+            System.out.print("Escolha uma opção: ");
+
+            int userInput = 0;
+
+            try {
+                userInput = Integer.parseInt(SCANNER.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Digite um valor numérico! " + e.getMessage());
+            }
+
+            switch (userInput) {
+                case 1:
+                    // TODO: Cadastrar novo pet
+                    break;
+                case 2:
+                    // TODO: Alterar pet cadastrado
+                    break;
+                case 3:
+                    // TODO: Remover pet cadastrado
+                    break;
+                case 4:
+                    // TODO: Listar pets
+                    break;
+                case 5:
+                    // TODO: Listar pets por critério
+                    break;
+                case 6:
+                    return;
+                default:
+                    System.out.println("Opção inválida! tente novamente.");
+                    break;
+            }
+
+            System.out.println();
         }
     }
 
